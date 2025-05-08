e9054834a18a35b51c3516306c6975318e273753
IlyaceBTL <ilyace.boutouil@gmail.com>
2025-05-08 12:51:09 +0200

    Adding model

---
+++ src/main/java/com/safetynet/alerts/model/Firestation.java
@@ -0,0 +1,31 @@
+package com.safetynet.alerts.model;
+
+public class Firestation {
+    String address;
+    String station;
+
+    public Firestation(String address, String station) {
+        this.address = address;
+        this.station = station;
+
+    }
+
+    public Firestation(){
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+}
