70ddd51295860d891a34d33290be944763290d1a
IlyaceBTL <ilyace.boutouil@gmail.com>
2025-05-16 11:14:10 +0200

    cleaning PersonService

--- src/main/java/com/safetynet/alerts/service/PersonService.java
+++ src/main/java/com/safetynet/alerts/service/PersonService.java
@@ -1,28 +1,19 @@
 package com.safetynet.alerts.service;
 
-import com.safetynet.alerts.dto.ChildAlertDTO;
-import com.safetynet.alerts.dto.CommunityEmailDTO;
-import com.safetynet.alerts.dto.PersonInfoDTO;
-import com.safetynet.alerts.model.MedicalRecords;
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.repository.PersonRepository;
-import com.safetynet.alerts.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class PersonService {
 
     private final PersonRepository personRepository;
-    private final MedicalRecordsService medicalRecordsService;
 
     @Autowired
-    public PersonService(PersonRepository personRepository, MedicalRecordsService medicalRecordsService) {
+    public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
-        this.medicalRecordsService = medicalRecordsService;
     }
 
     public Person createPerson(Person person) {
